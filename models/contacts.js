76cac9b632db697f71f3ceec8834914cd01aa1fd
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-09-22 19:34:13 +0200

    add

--- models/contacts.js
+++ models/contacts.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 const path = require("path");
 const { nanoid } = require("nanoid");
 
-const contactsPath = path.join(__dirname, "db", "contacts.json");
+const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
@@ -16,14 +16,15 @@ const getContactById = async (contactId) => {
 };
 
 const removeContact = async (contactId) => {
-  const data = await listContacts();
-  const index = data.findIndex((item) => item.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  const [result] = data.splice(index, 1);
-  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
-  return result;
+      const books = await listContacts();
+      const index = books.findIndex((item) => item.id === contactId);
+      console.log('index: ', index);
+      if (index === -1) {
+        return null;
+      }
+      const [result] = books.splice(index, 1);
+      await fs.writeFile(contactsPath, JSON.stringify(books, null, 2));
+      return result;
 };
 
 const addContact = async ({ name, email, phone }) => {
@@ -40,7 +41,14 @@ const addContact = async ({ name, email, phone }) => {
 };
 
 const updateContact = async (contactId, body) => {
-  
+  const data = await listContacts();
+  const index = data.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  data[index] = { contactId, ...body };
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return data[index];
 };
 
 module.exports = {
