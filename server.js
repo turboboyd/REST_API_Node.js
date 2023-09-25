57528729f431f23264d378fbf96cae06aa1d562b
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-09-25 17:41:46 +0200

    add

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://denisdaniv1:YrIHMR8FzSK7vgNh@contact.qwv6cwr.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3001);
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
