dbc686c91708f096855115604a41fd38c73e90f0
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-09-25 16:20:24 +0200

    Add

---
+++ Admin.js
@@ -0,0 +1,10 @@
+
+const admin = "denisdaniv1";
+const pasword = "YrIHMR8FzSK7vgNh";
+
+
+
+const DB_URl = `mongodb+srv://denisdaniv1:YrIHMR8FzSK7vgNh@contact.qwv6cwr.mongodb.net/?retryWrites=true&w=majority`;
+
+
+1;37
