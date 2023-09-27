185da6a8bc8379d410e1eb1945d25672f85b2458
Denis Daniv <113061164+turboboyd@users.noreply.github.com>
2023-09-27 12:10:24 +0200

    add

--- shemas/shemaContacts.js
+++ shemas/shemaContacts.js
@@ -2,22 +2,25 @@ const Joi = require("joi");
 
 const addSchemaErrorMessages = {
   "any.required": "missing required {{#label}} field",
+  "string.email": "invalid {{#label}} format",
+  "string.pattern.base":
+    "invalid {{#label}} format.{{#label}} format must: +380000000000 ",
   "object.min": "missing fields",
 };
 
 const addSchema = Joi.object()
   .when(Joi.object().min(1), {
     then: Joi.object({
-      name: Joi.string().required(),
-      email: Joi.string().email().required(),
+      name: Joi.string().required().label("Name"),
+      email: Joi.string().email().required().label("Email"),
       phone: Joi.string()
         .pattern(new RegExp("^[0-9+()\\-]*$"))
         .required()
-        ,
+        .label("Phone"),
     }),
   })
   .min(1)
-  .message( addSchemaErrorMessages);
+  .messages(addSchemaErrorMessages);
 
 module.exports = {
   addSchema,
